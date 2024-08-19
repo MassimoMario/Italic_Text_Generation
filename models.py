26cdfa1ec460b3c03b340abbaa163fcdf074baeb
Mariuolo <150280063+MassimoMario@users.noreply.github.com>
2024-08-19 16:07:38 +0200

    Added comments

--- models.py
+++ models.py
@@ -5,6 +5,29 @@ from torch.nn import functional as F
 
 
 class RNNcVAE(nn.Module):
+    ''' Class of a VAE where both Encoder and Decoder are RNNs, inherting from the nn.RNN module
+
+    Attributes
+    ----------
+    embedding_matrix : 2d torch tensor matrix from word2vec embedding
+    idx2word : vocabulary to pass from indices to word
+    num_classes : int, number of target classes
+    hidden_dim : int, dimension of RNNs hidden state
+    latent_dim : int, dimension of the VAE latent space
+    num_layers : int, number of RNNs layers
+    sos_token : torch tensor of the 'start of the sequence' token
+    vocab_size : int, number of unique tokens in the dataset
+
+    
+    Methods
+    -------
+    forward(x) : perform the forward pass of the VAE
+    reparametrization(mu, log_var) : perform the reparametrization trick
+    condition_on_label(z, label) : add the conditioning label to the latent variable
+    sample(label, len_sample, temperature) : inference for generating sequences
+    number_parameters() : returns the number of model parameters
+    '''
+
     def __init__(self, embedding_matrix, idx2word, num_classes, hidden_dim, latent_dim, num_layers, sos_token, vocab_size):
         super(RNNcVAE, self).__init__()
 
@@ -30,15 +53,33 @@ class RNNcVAE(nn.Module):
 
     
     def forward(self, x, label):
+        ''' Performs the VAE forward pass 
+        Input
+        -------
+        x : torch tensor with shape [Batch_size, Sequence_length], input sequence
+        label : torch tensor with shape [Batch_size, 3], conditioning label
+        
+
+        Returns
+        -------
+        reconstructed_sequence : torch tensor with shape [Batch_size, Sequence_length, Embedding_dimension] 
+        mu : torch tensor with shape [num_layers, batch_size, latent_dim]
+        logvar : torch tensor with shape [num_layers, batch_size, latent_dim]
+        '''
+
+        # embedding
         embedded_input = self.embedding(x)
         embedded_input = self.layer_norm(embedded_input)
 
+        # encoder pass
         _, hn = self.encoder(embedded_input)
 
+        # computing mu and logvar 
         mu = self.fc_mu(hn)
         logvar = self.fc_logvar(hn)
         z = self.reparametrization(mu, logvar)
 
+        # conditioning with label
         z = self.condition_on_label(z, label)
         z = self.fc_hidden(z)
 
@@ -47,11 +88,12 @@ class RNNcVAE(nn.Module):
         sos_token = self.embedding(sos_token)
         sos_token = self.layer_norm(sos_token)
 
+        # preparing decoder inputs
         decoder_input = torch.cat((sos_token, embedded_input), dim = 1)
         decoder_input = decoder_input[:,:-1,:]
 
+        # decoder pass and Linear layer to vocab_size dimensions
         reconstructed_sequence, _ = self.decoder(decoder_input, z)
-    
         reconstructed_sequence = self.fc(reconstructed_sequence)
         
         return reconstructed_sequence, mu, logvar
@@ -69,7 +111,7 @@ class RNNcVAE(nn.Module):
         
         Returns
         -------
-        mu + eps*std : torch tensor with the same shape as mu and log_var'''
+        mu + eps*std : torch tensor with the same shape as mu and log_var '''
         
         std = torch.exp(0.5*log_var)
         eps = torch.randn_like(std)
@@ -79,6 +121,20 @@ class RNNcVAE(nn.Module):
 
 
     def condition_on_label(self, z, label):
+        ''' Perform the conditioning of the VAE
+
+        Inputs
+        -------
+        z : torch tensor
+        label : torch tensor
+
+
+        Returns:
+        --------
+        z + projected_label : torch tensor
+        '''
+
+        # Linear layer to latent space dimensions
         projected_label = self.fc_condition(label)
         projected_label = projected_label.repeat((self.num_layers,1,1))
 
@@ -86,12 +142,29 @@ class RNNcVAE(nn.Module):
 
     
 
-    def sample(self, label, len_sample = 25, temperature = 1, sample_type = 'multinomial'):
+    def sample(self, label, len_sample = 25, temperature = 1):
+        ''' Function for inference and generating sentences
+
+        Inputs:
+        --------
+        label : torch tensor with shape [1,3]
+        len_sample : int, length of the generated sentence
+        temperature : int, temperature factor for sampling from softmax
+
+        Returns:
+        --------
+        ' '.join(sampled_text) : string of the generated sentence
+        perplexity : float, perplexity score
+        '''
+
+        # taking z from a Gaussian distribution
         z = torch.randn((self.num_layers, 1, self.latent_dim))
         perplexity = 0.0
 
         self.eval()
         with torch.no_grad():
+
+            # conditioning on label
             z = self.condition_on_label(z, label)
             z = self.fc_hidden(z)
 
@@ -103,14 +176,20 @@ class RNNcVAE(nn.Module):
 
 
             sampled_text = []
+
             # decoder pass where the input is the previous output
             output = sos_token
             for _ in range(len_sample):
                 outputs, _ = self.decoder(output, z)
                 outputs = self.fc(outputs)
                
+               # taking next token from the last RNN output
                 next_token = torch.multinomial(F.softmax(outputs[:,-1,:] / temperature, dim = -1), 1)
+
+                # computing log of probabilities
                 perplexity += torch.log(F.softmax(outputs[:,-1,:] / temperature, dim = -1)[0][next_token])
+
+                # appending next token to the sampled text
                 sampled_text.append(next_token)
                 next_token = self.embedding(next_token)
                 next_token = self.layer_norm(next_token)
@@ -118,9 +197,10 @@ class RNNcVAE(nn.Module):
                 output = torch.cat((output, next_token), dim=1)
        
 
-
+        # bringing sampled text to string with idx2word
         sampled_text = [self.idx2word[w.item()] for w in sampled_text]
 
+        # computing perplexity
         perplexity = torch.exp(- perplexity / len_sample).item()
         
         return ' '.join(sampled_text), perplexity
@@ -129,6 +209,7 @@ class RNNcVAE(nn.Module):
     
 
     def number_parameters(self):
+        ''' Function returning number of model parameters '''
 
         model_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
 
@@ -141,6 +222,8 @@ class RNNcVAE(nn.Module):
 
 
 class GRUcVAE(RNNcVAE, nn.Module):
+    ''' Class of a VAE where both Encoder and Decoder are RNNs with GRU units, inherting from the nn.GRU module and RNNcVAE class '''
+
     def __init__(self, embedding_matrix, idx2word, num_classes, hidden_dim, latent_dim, num_layers, sos_token, vocab_size):
         super().__init__(embedding_matrix, idx2word, num_classes, hidden_dim, latent_dim, num_layers, sos_token, vocab_size)
 
@@ -168,6 +251,29 @@ class GRUcVAE(RNNcVAE, nn.Module):
 
 
 class LSTMcVAE(nn.Module):
+    ''' Class of a VAE where Encoder is a LSTM and the Decoder is a GRU, inherting from the nn.LSTM and nn.GRU module
+
+    Attributes
+    ----------
+    embedding_matrix : 2d torch tensor matrix from word2vec embedding
+    idx2word : vocabulary to pass from indices to word
+    num_classes : int, number of target classes
+    hidden_dim : int, dimension of RNNs hidden state
+    latent_dim : int, dimension of the VAE latent space
+    num_layers : int, number of RNNs layers
+    sos_token : torch tensor of the 'start of the sequence' token
+    vocab_size : int, number of unique tokens in the dataset
+
+    
+    Methods
+    -------
+    forward(x) : perform the forward pass of the VAE
+    reparametrization(mu, log_var) : perform the reparametrization trick
+    condition_on_label(z, label) : add the conditioning label to the latent variable
+    sample(label, len_sample, temperature) : inference for generating sequences
+    number_parameters() : returns the number of model parameters
+    '''
+
     def __init__(self, embedding_matrix, idx2word, num_classes, hidden_dim, latent_dim, num_layers, sos_token, vocab_size):
         super(LSTMcVAE, self).__init__()
 
@@ -193,15 +299,33 @@ class LSTMcVAE(nn.Module):
 
     
     def forward(self, x, label):
+        ''' Performs the VAE forward pass 
+        Input
+        -------
+        x : torch tensor with shape [Batch_size, Sequence_length], input sequence
+        label : torch tensor with shape [Batch_size, 3], conditioning label
+        
+
+        Returns
+        -------
+        reconstructed_sequence : torch tensor with shape [Batch_size, Sequence_length, Embedding_dimension] 
+        mu : torch tensor with shape [num_layers, batch_size, latent_dim]
+        logvar : torch tensor with shape [num_layers, batch_size, latent_dim]
+        '''
+
+        # embedding
         embedded_input = self.embedding(x)
         embedded_input = self.layer_norm(embedded_input)
 
+        # encoder pass
         _, (hn, cn) = self.encoder(embedded_input)
 
+        # computing mu and logvar
         mu = self.fc_mu(hn)
         logvar = self.fc_logvar(hn)
         z = self.reparametrization(mu, logvar)
 
+        # conditioning on label
         z = self.condition_on_label(z, label)
         z = self.fc_hidden(z)
 
@@ -210,18 +334,12 @@ class LSTMcVAE(nn.Module):
         sos_token = self.embedding(sos_token)
         sos_token = self.layer_norm(sos_token)
 
+        # preparing decoder inputs
         decoder_input = torch.cat((sos_token, embedded_input), dim = 1)
         decoder_input = decoder_input[:,:-1,:]
 
+        # decoder pass and Linear layer to vocab_size dimensions
         reconstructed_sequence, _ = self.decoder(decoder_input, z)
-        '''# reconstructing sequence through the decoder giving z as hidden state for each time step
-        reconstructed_sequence = []
-        for t in range(x.shape[1]):
-            outputs, _ = self.decoder(decoder_input[:,:t+1,:], z)
-            reconstructed_sequence.append(outputs[:,-1,:].unsqueeze(1))
-
-        # concatenating reconstructed words and push them into vocab_size dimensions
-        reconstructed_sequence = torch.cat(reconstructed_sequence, dim=1)'''
         reconstructed_sequence = self.fc(reconstructed_sequence)
         
         return reconstructed_sequence, mu, logvar
@@ -249,6 +367,20 @@ class LSTMcVAE(nn.Module):
 
 
     def condition_on_label(self, z, label):
+        ''' Perform the conditioning of the VAE
+
+        Inputs
+        -------
+        z : torch tensor
+        label : torch tensor
+
+
+        Returns:
+        --------
+        z + projected_label : torch tensor
+        '''
+
+        # Linear layer to latent space dimensions
         projected_label = self.fc_condition(label)
         projected_label = projected_label.repeat((self.num_layers,1,1))
 
@@ -256,12 +388,29 @@ class LSTMcVAE(nn.Module):
 
     
 
-    def sample(self, label, len_sample = 25, temperature = 1, sample_type = 'multinomial'):
+    def sample(self, label, len_sample = 25, temperature = 1):
+        ''' Function for inference and generating sentences
+
+        Inputs:
+        --------
+        label : torch tensor with shape [1,3]
+        len_sample : int, length of the generated sentence
+        temperature : int, temperature factor for sampling from softmax
+
+        Returns:
+        --------
+        ' '.join(sampled_text) : string of the generated sentence
+        perplexity : float, perplexity score
+        '''
+
+        # z from Gaussian distribution
         z = torch.randn((self.num_layers, 1, self.latent_dim))
         perplexity = 0.0
 
         self.eval()
         with torch.no_grad():
+
+            # conditioning on label
             z = self.condition_on_label(z, label)
             z = self.fc_hidden(z)
 
@@ -273,14 +422,20 @@ class LSTMcVAE(nn.Module):
 
 
             sampled_text = []
+
             # decoder pass where the input is the previous output
             output = sos_token
             for _ in range(len_sample):
                 outputs, _ = self.decoder(output, z)
                 outputs = self.fc(outputs)
         
+                # taking next token from last output
                 next_token = torch.multinomial(F.softmax(outputs[:,-1,:] / temperature, dim = -1), 1)
+
+                # computing log of probabilities
                 perplexity += torch.log(F.softmax(outputs[:,-1,:] / temperature, dim = -1)[0][next_token])
+
+                # appending next token to the sampled text
                 sampled_text.append(next_token)
                 next_token = self.embedding(next_token)
                 next_token = self.layer_norm(next_token)
@@ -288,19 +443,19 @@ class LSTMcVAE(nn.Module):
                 output = torch.cat((output, next_token), dim=1)
        
 
-      
-        
-
+        # bringing sampled text to string with idx2word
         sampled_text = [self.idx2word[w.item()] for w in sampled_text]
 
-
+        # computing average perplexity
         perplexity = torch.exp(- perplexity / len_sample).item()
+
         return ' '.join(sampled_text), perplexity
     
 
     
 
     def number_parameters(self):
+        ''' Function returning number of model parameters '''
 
         model_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
 
@@ -315,6 +470,19 @@ class LSTMcVAE(nn.Module):
 
 
 class CNNClassifier(nn.Module):
+    '''  Class of a CNN Classifier for text, made up of Conv2d layers
+
+    Attributes
+    ----------
+    embedding_matrix : 2d torch tensor matrix from word2vec embedding
+    num_classes : int, number of classes
+    num_filters : int, number of filters in the Conv2d layer
+    kernel_sizes : list of int, sizes of kernels in Conv2d layers
+
+    Methods
+    ----------
+    forward(x) : forward pass of the Classifier'''
+
     def __init__(self, embedding_matrix, num_classes, num_filters, kernel_sizes):
         super(CNNClassifier, self).__init__()
         self.embedding_dim = embedding_matrix.shape[1]
@@ -324,10 +492,26 @@ class CNNClassifier(nn.Module):
         self.fc = nn.Linear(num_filters * len(kernel_sizes), num_classes)
         
     def forward(self,x):
+        ''' Forward pass function
+        
+        Input
+        ----------
+        x : 2D torch tensor tensor, input sentence with shape [Batch size, Sequence length]
+        
+        Returns
+        ----------
+        out : 2D torch tensor with probabilities for every class'''
+
+        # Word Embedding
         x = self.embedding(x)
         x = x.unsqueeze(1)
+
+         # Convolutional layers and Max pool
         conv_results = [F.relu(conv(x)).squeeze(3) for conv in self.conv_layers]
         pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conv_results]
+
+        # Concatenation of pooled output and Linear layer to num_class dimensions
         cat = torch.cat(pooled, dim = 1)
         out = self.fc(cat)
+        
         return F.softmax(out, dim=-1)
--- training_function.py
+++ training_function.py
@@ -10,16 +10,20 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def vae_loss(recon_x, x, mu, logvar, l_kl = 0.05, loss_fn = nn.CrossEntropyLoss()):
-    ''' Function computing loss function for classification
+    ''' Function computing loss function for the VAE
     
     Inputs
     ---------
-    pred_labels : 3D torch tensor with predicted labels with shape [1, Batch size, 3]
-    labels : 2D torch tensor with ground truth labels with shape [Batch size, 3]
+    recon_x : torch tensor with shape [Batch_size, Sequence_length, Embedding_dim]
+    x : torch tensor with shape [Batch_size, Sequence_length]
+    mu : torch tensor with shape [num_layers, batch_size, latent_dim]
+    logvar : torch tensor with shape [num_layers, batch_size, latent_dim]
+    l_kl : float, factor for KL Divergence
+    loss_fn : loss function, initialized with nn.CrossEntropyLoss()
     
     Returns
     ---------
-    L : float, loss value '''
+    L + l_kl*KLD: float, reconstruction loss + KL Divergence'''
 
     L = loss_fn(recon_x.reshape((recon_x.size(0)*recon_x.size(1),recon_x.size(2))), x.view(-1))
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
@@ -45,7 +49,7 @@ def training(model, train_loader, val_loader, num_epochs, lr = 4e-4, title = 'Tr
     
     Returns
     --------
-    train_losses : list with train loss values '''
+    None '''
 
     params = list(model.parameters())
 
@@ -154,9 +158,20 @@ def training(model, train_loader, val_loader, num_epochs, lr = 4e-4, title = 'Tr
 
 
 def CNN_loss(y_s, labels, loss_fn=nn.CrossEntropyLoss()):
-    L_mul_s = loss_fn(y_s, labels)
+    ''' Function computing loss for CNN Classifier
+    
+    Inputs:
+    --------
+    y_s : torch tensor with shape [Batch_size, 3], predicted labels
+    labels : torch tensor with shape [Batch_size, 3], ground truth labels 
+    
+    Returns:
+    --------
+    L_classif : float, loss value'''
 
-    return L_mul_s
+    L_classif = loss_fn(y_s, labels)
+
+    return L_classif
 
 
 
@@ -167,6 +182,19 @@ def CNN_loss(y_s, labels, loss_fn=nn.CrossEntropyLoss()):
 
 
 def train_CNN(style_classif, train_loader, val_loader, num_epochs, lr = 4e-4):
+    ''' Function for training the CNN Classifier
+    
+    Inputs:
+    --------
+    style_classif : istance of CNNClassifier
+    train_loader : istance of torch Dataloader with training data and labels
+    val_loader : istance of torch Dataloader with validation data and labels
+    num_epochs : int, number of epochs
+    lr : float, learning rate for Adam optimizer
+
+    Returns:
+    average_losses : list of training loss values
+    '''
     params = list(style_classif.parameters())
 
     optimizer = torch.optim.Adam(params, lr = lr)
